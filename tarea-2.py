ba750a5e37dfad2d58a2364f25642cd94bee96dd
Leonardo Luque <64507345+LeonardoLLP@users.noreply.github.com>
2022-03-09 17:59:22 +0100

    First instance of code. Not tested

--- tarea-2.py
+++ tarea-2.py
@@ -1,6 +1,9 @@
+# La manera en la que lo vamos a plantear es la siguiente: primero se coloca un numero, que actuara como pivote.
+# Posteriormente colocamos el siguiente número que cumpla todas las condiciones. Después, otro más, y así sucesivamente.
+
 from random import randint
 
-t = list(range(1, 11))
+t = [i + 1 for i in range(10)]
 
 conditions = [(randint(1, 10), randint(1, 10)) for _ in range(5)]
 conditions.sort()
@@ -8,13 +11,47 @@ print(conditions)
 
 t = [i+1 for i in range(10)]
 
-def check(condition: tuple) -> bool:
+
+def indexes(condition: tuple):
     i, j = condition
     a = t.index(i)
     b = t.index(j)
+    return a, b
+
+def check(condition: tuple) -> bool:
+    a, b = indexes(condition)
     if a <= b:
         return True
     else:
         return False
 
-print(check(conditions[1]))
+def check_all(conditions: list):
+    for condition in conditions:
+        if not check(condition):
+            return False
+        return True
+
+
+
+
+
+
+
+all_conditions_met = check_all(conditions)
+i = 0
+while not all_conditions_met:
+    for condition in conditions:
+        if not check(condition):
+            first, second = indexes(condition)
+            second_value = t.pop(second)  #? No es igual a condition[1] ???
+            t.insert(second_value, first + 1)
+
+    all_conditions_met = check_all(conditions)
+
+    #! Breaker to try code
+    i += 1
+    if i >= 10000:
+        print("Too long")
+        break
+
+print(t)
